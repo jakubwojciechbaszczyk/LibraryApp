2c03ce43a0e472ecfa56349f183137bd7753777a
JakubBaszczyk <jakubwojciechbaszczyk@gmail.com>
2024-01-21 17:59:33 +0100

    change name parameter in insert abstract method

--- app/src/main/java/com/example/libraryapp/data/person/PersonDataSource.kt
+++ app/src/main/java/com/example/libraryapp/data/person/PersonDataSource.kt
@@ -3,6 +3,6 @@ package com.example.libraryapp.data.person
 import kotlinx.coroutines.flow.Flow
 
 interface PersonDataSource {
-    suspend fun insert(person: List<Person>)
+    suspend fun insert(persons: List<Person>)
     fun allPerson(): Flow<List<Person>>
 }
